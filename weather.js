aa4ea0e5f647185d814c1286cc0f99adf2807e86
hbh1221 <Helen.bao1@kellogg.northwestern.edu>
2018-02-19 21:36:53 -0600

    Add files via upload

--- weather.js
+++ weather.js
@@ -13,7 +13,7 @@
 // NOTES AND HINTS
 
 // All of the work of grabbing data from the Dark Sky API is already done
-// for you! Your task is to take that data, transform it into HTML, and 
+// for you! Your task is to take that data, transform it into HTML, and
 // insert it into the document. All of your work begins on line 47!
 
 // Each day of the forecast should use HTML markup similar to:
@@ -37,7 +37,7 @@
 // .append() appends a string (containing HTML) to a jQuery DOM object
 
 let handleWeatherResponse = function(response) {
-  // leave these two lines alone; they allow for the inspection of 
+  // leave these two lines alone; they allow for the inspection of
   // the response object in the browser console (try typing "response"
   // in the Chrome JavaScript console!)
   console.log(response)
@@ -45,6 +45,35 @@ let handleWeatherResponse = function(response) {
 
   // **** your code starts here - don't modify anything else. you will be sad.
 
+  let current_weather = response.currently;
+  let forecast_weather = response.daily.data;
+
+  $("#current-conditions-icon").empty();
+  $("#current-conditions-text").empty();
+  $(".forecast").empty();
+
+  // Adds the icon for the current weather
+  let current_icon_html = icon(current_weather.icon);
+  $("#current-conditions-icon").append(current_icon_html);
+
+  // Adds the text for the summary of the current weather
+  let current_summary_html = current_weather.summary;
+  $("#current-conditions-text").append(current_summary_html);
+
+  // Adds the row with weather forecast for 6 days
+  for (let i=0; i<6; i++){
+    let html = '<div class="col">';
+    // html = html + '<h4>' +
+    html = html + '<h3>' + icon(forecast_weather[i].icon) + '</h3>';
+    html = html + '<h4>' + Math.round(forecast_weather[i].temperatureMax) + " | " + Math.round(forecast_weather[i].temperatureMin) + '</h4>';
+    html = html + '<h5>' + forecast_weather[i].summary + '</h5>';
+    html = html +'</div>';
+    $(".forecast").append(html);
+  }
+
+  $(".current").fadeIn(500);
+  $(".forecast").fadeIn(4000);
+  
   // *** your code ends here -- really.
 };
 
